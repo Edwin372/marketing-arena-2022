ad6ea6ad1ae24b13b7d885b057fe3c43e2ccf8c2
Chau <tranhoaichau.00@gmail.com>
2022-04-13 23:20:27 +0700

    add scroll animation

--- components/landing/index.js
+++ components/landing/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Image from 'next/image'
+import { useLax, useLaxElement } from 'use-lax';
 import lax from 'lax.js'
 import cloud1 from '../../public/svg/cloud1.svg'
 import cloud2 from '../../public/svg/cloud2.svg'
@@ -7,41 +8,52 @@ import cloud3 from '../../public/svg/cloud3.svg'
 import title from '../../public/svg/title.svg'
 import plane from '../../public/svg/plane.svg'
 
-class Landing extends Component {
-    state = {  }
-    render() {
+function Landing() {
+    useLax({
+        drivers: [
+            {
+                name: 'scrollY',
+                getValueFn: () => window.scrollY,
+            },
+        ],
+    });
         return (
             <div id='landing' className="session">
-                <Image
-                    className={`floating-image cloud1`}
-                    src={cloud1}
-                    width={501.65}
-                    height={308.26}
-                    layout={"raw"}
-                />
-                <Image
-                    className={`floating-image cloud2`}
-                    src={cloud2}
-                    width={786}
-                    height={326}
-                    layout={"raw"}
-                />
-                <Image
-                    className={`floating-image cloud3`}
-                    src={cloud3}
-                    width={1202}
-                    height={563}
-                    layout={"raw"}
-                />
-                <Image
-                    className={`floating-image plane`}
-                    src={plane}
-                    width={1344}
-                    height={486}
-                    priority={true}
-                    layout={"raw"}
 
-                />
+                    <Image
+                        className={`floating-image cloud1`}
+                        src={cloud1}
+                        width={501.65}
+                        height={308.26}
+                        layout={"raw"}
+                    />
+                 <div className={`lax lax_preset_slideX:852:150 floating-image cloud2`}>
+                    <Image
+                        src={cloud2}
+                        width={786}
+                        height={326}
+                        layout={"raw"}
+                     />
+                </div>
+                <div className={`lax lax_preset_slideX:852:100 floating-image cloud3 `}>
+                    <Image
+                        src={cloud3}
+                        width={1202}
+                        height={563}
+                        layout={"raw"}
+                    />
+                </div>
+
+                <div className={`lax lax_preset_slideX:852:-210 floating-image plane`}>
+                    <Image
+                        src={plane}
+                        width={1344}
+                        height={486}
+                        priority={true}
+                        layout={"raw"}
+
+                    />
+                </div>
                 <Image
                     className={`floating-image title`}
                     src={title}
@@ -54,7 +66,6 @@ class Landing extends Component {
             </div>
 
         );
-    }
 }
 
 export default Landing ;
