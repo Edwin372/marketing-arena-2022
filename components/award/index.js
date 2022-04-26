0426434345aa2a0a0efa481cb5a6f0fda26dbcfe
Chau <tranhoaichau.00@gmail.com>
2022-04-26 23:37:50 +0700

    add award session

--- components/award/index.js
+++ components/award/index.js
@@ -1,13 +1,96 @@
 import React, { Component } from 'react';
+import Image from 'next/image'
+import champion from '../../public/images/champion.png'
+import trueRunnerUp from '../../public/images/truerunnerup.png'
+import runnerUp from '../../public/images/runnerup.png'
+import championAward from '../../public/images/champprize.png'
+import trueRunnerUpAward from '../../public/images/truerunnerupprize.png'
+import runnerUpAward from '../../public/images/runnerupprize.png'
+import awardTitle from '../../public/images/awardtitle.png'
+import circle4 from '../../public/images/circle4.png'
+import circle5 from '../../public/images/circle5.png'
+import circle6 from '../../public/images/circle6.png'
+import circle7 from '../../public/images/circle7.png'
+import circle8 from '../../public/images/circle8.png'
 
 class Award extends Component {
-    state = {  }
+    state = {}
     render() {
         return (
             <div id="award" className="session">
-                Award
-            </div>
+                <Image className={'award-title'} src={awardTitle} layout={'raw'}/>
+                <div
+                    id={'first'}
+                    className={'sub-session'}
+                >
+                    <Image
+                        className={'champion'}
+                        src={champion}
+                        layout={'raw'}
+                    />
+                    <Image
+                        className={'champion-award'}
+                        src={championAward}
+                        layout={'raw'}
+                    />
+                    <Image
+                        className={'floating-image circle4'}
+                        src={circle4}
+                        layout={'raw'}
+                    />
+                    <Image
+                        className={'floating-image circle5'}
+                        src={circle5}
+                        layout={'raw'}
+                    />
+                </div>
+                <div
+                    id={'second'}
+                    className={'sub-session'}
+                >
+                    <Image
+                        className={'true-runner-up'}
+                        src={trueRunnerUp}
+                        layout={'raw'}
+                    />
+                    <Image
+                        className={'true-runner-up-award'}
+                        src={trueRunnerUpAward}
+                        layout={'raw'}
+                    />
+                    <Image
+                        className={'floating-image circle6'}
+                        src={circle6}
+                        layout={'raw'}
+                    />
+                    <Image
+                        className={'floating-image circle7'}
+                        src={circle7}
+                        layout={'raw'}
+                    />
+                </div>
 
+                <div
+                    id={'third'}
+                    className={'sub-session'}
+                >
+                    <Image
+                        className={'runner-up'}
+                        src={runnerUp}
+                        layout={'raw'}
+                    />
+                    <Image
+                        className={'runner-up-award'}
+                        src={runnerUpAward}
+                        layout={'raw'}
+                    />
+                    <Image
+                        className={'floating-image circle8'}
+                        src={circle8}
+                        layout={'raw'}
+                    />
+                </div>
+            </div>
         );
     }
 }
--- components/rules/index.js
+++ components/rules/index.js
@@ -6,6 +6,8 @@ import cloud8 from '../../public/svg/cloud8.svg'
 import cloud9 from '../../public/svg/cloud9.svg'
 import cloud10 from '../../public/svg/cloud10.svg'
 import pilot from '../../public/images/pilot.png'
+import cloud11 from '../../public/images/cloud11.png'
+import rulesSession from '../../public/images/rulesession.png'
 
 class Rules extends Component {
     state = {  }
@@ -15,53 +17,81 @@ class Rules extends Component {
                 id="rules"
                 className="session"
             >
+                <div
+                    id="rules-board"
+                    className="session"
+                >
+                    <Image
+                        src={cloud11}
+                        className={`floating-image cloud11`}
+                        width={2456}
+                        height={656}
+                        layout={'raw'}
+                    />
+                    <Image
+                        src={cloud11}
+                        className={`floating-image cloud12`}
+                        width={2456}
+                        height={656}
+                        layout={'raw'}
+                    />
+                    <Image
+                        className={`floating-image rule-board`}
+                        src={ruleBoard}
+                        width={1074}
+                        height={777}
+                        priority={true}
+                        quality={100}
+                        layout={"raw"}
+                    />
+                    <Image
+                        className={'floating-image cloud-background'}
+                        src={cloudBackground}
+                        layout={"raw"}
+                        priority={true}
+                    />
+                    <div className={`floating-image cloud8`}>
+                        <Image
+                            src={cloud8}
+                            layout={"raw"}
+                            width={1468}
+                            height={443}
+                        />
+                    </div>
+                    <div className={'floating-image cloud9'}>
+                        <Image
+                            src={cloud9}
+                            layout={"raw"}
+                            width={516}
+                            height={248}
+                        />
+                    </div>
+                    <div className={'floating-image cloud10'} >
+                        <Image
+                            src={cloud10}
+                            layout={"raw"}
+                            width={859}
+                            height={269}
+                        />
+                    </div>
+                    <div className={'pilot'} >
+                        <Image
+                            src={pilot}
+                            layout={"raw"}
+                            width={1519}
+                            height={853}
+                        />
+                    </div>
+
+                </div>
                 <Image
-                    className={`floating-image rule-board`}
-                    src={ruleBoard}
-                    width={1074}
-                    height={777}
                     priority={true}
                     quality={100}
-                    layout={"raw"}
+                    height={1461}
+                    src={rulesSession}
+                    className={'rule-session'}
+                    layout={'raw'}
                 />
-                <Image
-                    className={'floating-image cloud-background'}
-                    src={cloudBackground}
-                    layout={"raw"}
-                    priority={true}
-                />
-                <div className={`floating-image cloud8`}>
-                    <Image
-                        src={cloud8}
-                        layout={"raw"}
-                        width={1468}
-                        height={443}
-                    />
-                </div>
-                <div className={'floating-image cloud9'}>
-                    <Image
-                        src={cloud9}
-                        layout={"raw"}
-                        width={516}
-                        height={248}
-                    />
-                </div>
-                <div className={'floating-image cloud10'} >
-                    <Image
-                        src={cloud10}
-                        layout={"raw"}
-                        width={859}
-                        height={269}
-                    />
-                </div>
-                <div className={'pilot'} >
-                    <Image
-                        src={pilot}
-                        layout={"raw"}
-                        width={1519}
-                        height={853}
-                    />
-                </div>
 
             </div>
 
