e48ae21e3c1fe99ae92f9d5f5f1583f2aeb8570e
Chau <tranhoaichau.00@gmail.com>
2022-04-29 16:21:49 +0700

    add loader screen

--- components/loading/index.js
+++ components/loading/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Image from "next/image";
 import loaderIcon from '../../public/svg/loader.svg'
+import creatio from '../../public/svg/creatio.svg'
+
 class Footer extends Component {
     state = {  }
     render() {
         return (
             <div id="loading" className="floating-image">
-                <Image priority={true} src={loaderIcon} layout={'raw'} className={'loader-icon'}/>
+                <Image width={400} height={400}  data-aos={"fade-up"} priority={true} src={creatio} layout={'raw'} className={'loader-logo'}/>
+                <Image data-aos={"fade-down"} priority={true} src={loaderIcon} layout={'raw'} className={'loader-icon'}/>
 
             </div>
 
--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,9 @@ import "aos/dist/aos.css";
 export default function Home() {
     const [loading, setLoading] = useState(true);
     useEffect(() => {
+        setTimeout(() => {
             setLoading(false);
+        }, 5000);
     }, []);
     useEffect(() => {
         AOS.init();
