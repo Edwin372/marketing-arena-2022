5eb55e2097bb21d9a928acf5986760b4f3a0d175
Chau <tranhoaichau.00@gmail.com>
2022-05-02 20:39:02 +0700

    done

--- components/footer/index.js
+++ components/footer/index.js
@@ -10,7 +10,12 @@ class Footer extends Component {
                 <div id={"black-background"}></div>
                 <div id={"footer-text"}>
                     <Image src={contactTitle} layout={'raw'} className={'contact-title'}/>
-                    <p className={'footer-p'}><span>HOTLINE: </span>0914481131</p>
+                    <p className={'footer-p'}><span>HOTLINE GIẢI ĐÁP THẮC MẮC VỀ:</span></p>
+                    <p className={'footer-p'}>
+                        Format, đề bài: 0369 772 729 (Ms. Ngoc Loan - Leader of SP Department) <br/>
+                        Đăng ký dự thi: 0914 750 893  (Ms. Lan Anh - Leader of HR Department), <br/>
+                        0914 381 131 (Ms. Thuy Vi - Project Leader) <br/>
+                    </p>
                     <p className={'footer-p'}><span>EMAIl: </span>marketingarena2022@gmail.com</p>
                     <p className={'footer-p'}><span>FANPAGE: </span><a className={'footer-a'} href={'https://www.facebook.com/creatioftu2'}>https://www.facebook.com/creatioftu2</a></p>
                     <p className={'footer-p'}><span>WEBSITE: </span><a className={'footer-a'} href={'https://marketingarena2022.com'}>marketingarena2022.com</a></p>
